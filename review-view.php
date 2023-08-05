91d1fb9eb3a8f245aeb82998fba8097e5420e172
ShaLijiao <shalijiao@gmail.com>
2023-08-05 16:20:05 +0800

    Update review-view.php

--- review-view.php
+++ review-view.php
@@ -11,12 +11,24 @@ $post_data = json_decode($req_body, true);
 $reviewId = isset($post_data['reviewId']) ? $post_data['reviewId'] : '';
 $comment = isset($post_data['comment']) ? $post_data['comment'] : '';
 
-$sql = "SELECT * FROM r_reviews WHERE reviewId = '$reviewId'";
+$sql = "SELECT * FROM r_reviews r , r_user u ,r_restaurant t
+WHERE r.userId=u.userId,
+AND r.restaurantId= t.restaurantId,
+AND r.reviewId = '$reviewId'";
+
   $result = mysqli_query($db->myconn, $sql);
-  if ($result){
-    
-    $response = array("status" => 1, "message" => "review details list successful.");
-   echo json_encode($response);
+  if (mysqli_num_rows($result) > 0) {
+    // has found matching user 
+    $row = mysqli_fetch_assoc($result);
+      $response = array(
+          "status" => 1,
+          "message" => "review details list successful.",
+          "restaurantName" => (int)$row["t.name"],
+          "userName" => (int)$row["u.name"],
+          "comment" => $row["r.comment"],
+          "date" => (int)$row["r.date"],
+      );
+      echo json_encode($response);
  } else {
    // request failed
    $response = array("status" => 0, "message" => "review details list failed.");
