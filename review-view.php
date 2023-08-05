458a2d5e23b560136dcd0ab392c6f12589353bb4
ShaLijiao <shalijiao@gmail.com>
2023-08-05 15:56:55 +0800

    Update review-view.php

--- review-view.php
+++ review-view.php
@@ -8,5 +8,18 @@ $db->connect();
 $req_body= file_get_contents('php://input');
 $post_data = json_decode($req_body, true);
 
+$reviewId = isset($post_data['reviewId']) ? $post_data['reviewId'] : '';
+$comment = isset($post_data['comment']) ? $post_data['comment'] : '';
 
+$sql = "SELECT * FROM r_reviews WHERE reviewId = '$reviewId'";
+  $result = mysqli_query($db->myconn, $sql);
+  if ($result){
+    
+    $response = array("status" => 1, "message" => "review details list successful.");
+   echo json_encode($response);
+ } else {
+   // request failed
+   $response = array("status" => 0, "message" => "review details list failed.");
+   echo json_encode($response);
+ }
 ?>
