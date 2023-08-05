0e0f09114cc8563750db5cdbea373eef8180d1e7
ShaLijiao <shalijiao@gmail.com>
2023-08-05 17:00:41 +0800

    Update reviews.php

---
+++ reviews.php
@@ -0,0 +1,37 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+require_once __DIR__ . "/dbConnect.php";
+$db = new DB_CONNECT();
+$db->connect();
+
+class Review {
+    public static function generateReview ($row) {
+        return [
+            "reviewId " => (int)$row["reviewId"],
+            "username" => $row["username"],
+            "restaurantName" => $row["name"],
+            "comment" => $row["comment"],
+            "date" => $row["date"]
+        ];
+    }
+}
+
+$sql = "SELECT * FROM r_reviews r, r_users u, r_restaurants t 
+        WHERE r.userId = u.userId
+        AND r.restaurantId = t.restaurantId";
+
+$result = mysqli_query($db->myconn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    $data = [];
+    foreach ($result as $row) {
+        $rest_db = Review::generateReview($row);
+        $data[] = $rest_db;
+    }
+    $response = array("status" => 1, "data" => $data);
+    echo json_encode($response);
+} else {
+    $response = array("status" => 0, "message" => "No data found");
+    echo json_encode($response);
+}
+?>
